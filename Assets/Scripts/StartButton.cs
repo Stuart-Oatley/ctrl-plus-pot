d01b743eac57aac9cef29c8829902ac8c86d51c9
Stuart-Oatley <55748247+Stuart-Oatley@users.noreply.github.com>
2020-03-23 15:41:40 +0000

    menu start

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menuButtons;
+
+    [SerializeField]
+    Animator camAnimator;
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Hands"))
+        {
+            menuButtons.SetActive(false);
+            camAnimator.SetBool("GoFromMenuToWheel", true);
+        }
+    }
+}
