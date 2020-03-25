08c00281665f66e3fe04905670e698c301dca252
clburns99 <47043887+clburns99@users.noreply.github.com>
2020-03-25 21:02:37 +0000

    Start Menu fix

---
+++ Assets/Scripts/CatherineScripts/Menu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject menuButtons;
+
+    [SerializeField]
+    Animator camAnimator;
+
+    private void Start()
+    {
+        if(gameObject.name == "Menu")
+        {
+            StartCoroutine(menuWait(3));
+        }
+    }
+
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(gameObject.name)
+        {
+            case "StartButton":
+                if (collision.collider.CompareTag("Hands"))
+                {
+                    menuButtons.SetActive(false);
+                    camAnimator.SetBool("GoFromMenuToWheel", true);
+                }
+                break;
+            case "PotsButton":
+                if (collision.collider.CompareTag("Hands"))
+                {
+                    menuButtons.SetActive(false);
+                    Debug.Log("pots");
+                    // Load pots
+                }
+                break;
+            case "QuitButton":
+                if (collision.collider.CompareTag("Hands"))
+                {
+                    Debug.Log("quit");
+                    Application.Quit();
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    IEnumerator menuWait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        menuButtons.SetActive(true);
+    }
+}
